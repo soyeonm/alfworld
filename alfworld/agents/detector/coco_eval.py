bf01533a664e29c2158d1370d7e605778ccc1c88
So Yeon Min <soyeonmin@Soui-MacBookPro.local>
2021-07-29 16:44:47 +0900

    m

--- alfworld/agents/detector/coco_eval.py
+++ alfworld/agents/detector/coco_eval.py
@@ -13,7 +13,7 @@ import pycocotools.mask as mask_util
 
 from collections import defaultdict
 
-import alfworld.agents.detector.utils
+import alfworld.agents.detector.utils as utils
 
 
 class CocoEvaluator(object):
