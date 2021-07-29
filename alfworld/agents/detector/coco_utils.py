dad90ab0acbb5576dcb9b042611fd4ea06705b25
So Yeon Min <soyeonmin@Soui-MacBookPro.local>
2021-07-29 16:11:32 +0900

    m

--- alfworld/agents/detector/coco_utils.py
+++ alfworld/agents/detector/coco_utils.py
@@ -158,45 +158,45 @@ def convert_to_coco_api(ds):
         #print("img idx is ", img_idx)
         #try:
         img, targets = ds[img_idx]
-        if img is not None:
+        #if img is not None:
             #pickle.dump(targets, open("targets.p", "wb"))
-            image_id = targets["image_id"].item()
-            img_dict = {}
-            img_dict['id'] = image_id
-            img_dict['height'] = img.shape[-2]
-            img_dict['width'] = img.shape[-1]
-            dataset['images'].append(img_dict)
-            bboxes = targets["boxes"]
-            bboxes[:, 2:] -= bboxes[:, :2]
-            bboxes = bboxes.tolist()
-            labels = targets['labels'].tolist()
-            areas = targets['area'].tolist()
-            iscrowd = targets['iscrowd'].tolist()
+        image_id = targets["image_id"].item()
+        img_dict = {}
+        img_dict['id'] = image_id
+        img_dict['height'] = img.shape[-2]
+        img_dict['width'] = img.shape[-1]
+        dataset['images'].append(img_dict)
+        bboxes = targets["boxes"]
+        bboxes[:, 2:] -= bboxes[:, :2]
+        bboxes = bboxes.tolist()
+        labels = targets['labels'].tolist()
+        areas = targets['area'].tolist()
+        iscrowd = targets['iscrowd'].tolist()
+        if 'masks' in targets:
+            masks = targets['masks']
+            # make masks Fortran contiguous for coco_mask
+            masks = masks.permute(0, 2, 1).contiguous().permute(0, 2, 1)
+        if 'keypoints' in targets:
+            keypoints = targets['keypoints']
+            keypoints = keypoints.reshape(keypoints.shape[0], -1).tolist()
+        num_objs = len(bboxes)
+        for i in range(num_objs):
+            ann = {}
+            ann['image_id'] = image_id
+            ann['bbox'] = bboxes[i]
+            ann['category_id'] = labels[i]
+            categories.add(labels[i])
+            ann['area'] = areas[i]
+            ann['iscrowd'] = iscrowd[i]
+            ann['id'] = ann_id
             if 'masks' in targets:
-                masks = targets['masks']
-                # make masks Fortran contiguous for coco_mask
-                masks = masks.permute(0, 2, 1).contiguous().permute(0, 2, 1)
+                ann["segmentation"] = coco_mask.encode(masks[i].numpy())
             if 'keypoints' in targets:
-                keypoints = targets['keypoints']
-                keypoints = keypoints.reshape(keypoints.shape[0], -1).tolist()
-            num_objs = len(bboxes)
-            for i in range(num_objs):
-                ann = {}
-                ann['image_id'] = image_id
-                ann['bbox'] = bboxes[i]
-                ann['category_id'] = labels[i]
-                categories.add(labels[i])
-                ann['area'] = areas[i]
-                ann['iscrowd'] = iscrowd[i]
-                ann['id'] = ann_id
-                if 'masks' in targets:
-                    ann["segmentation"] = coco_mask.encode(masks[i].numpy())
-                if 'keypoints' in targets:
-                    ann['keypoints'] = keypoints[i]
-                    ann['num_keypoints'] = sum(k != 0 for k in keypoints[i][2::3])
-                dataset['annotations'].append(ann)
-                ann_id += 1
-            s_count+=1
+                ann['keypoints'] = keypoints[i]
+                ann['num_keypoints'] = sum(k != 0 for k in keypoints[i][2::3])
+            dataset['annotations'].append(ann)
+            ann_id += 1
+        s_count+=1
         #except:
         #    print("img is ", img)
     dataset['categories'] = [{'id': i} for i in sorted(categories)]
