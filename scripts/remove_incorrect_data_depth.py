9ff7e9ffad3620c6d58e0cfb0cf8ad95652ce4ed
So Yeon Min <soyeonmin@Soui-MacBookPro.local>
2021-08-10 05:27:22 +0900

    m

---
+++ scripts/remove_incorrect_data_depth.py
@@ -0,0 +1,104 @@
+import argparse
+from PIL import Image
+import numpy as np
+import os
+import pickle
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--data_path", type=str, required=True)
+args = parser.parse_args()
+
+def png_only(file_list):
+	return [f for f in file_list if f[-4:] == '.png']
+
+root = args.data_path
+
+kitchen_path = os.path.join(root, 'kitchen', 'images')
+living_path = os.path.join(root, 'living', 'images')
+bedroom_path = os.path.join(root, 'bedroom', 'images')
+bathroom_path = os.path.join(root, 'bathroom', 'images')
+#
+kitchen_name_only = png_only(list(sorted(os.listdir(kitchen_path))))
+living_name_only = png_only(list(sorted(os.listdir(living_path))))
+bedroom_name_only = png_only(list(sorted(os.listdir(bedroom_path))))
+bathroom_name_only = png_only(list(sorted(os.listdir(bathroom_path))))
+#
+kitchen = [os.path.join(kitchen_path, f) for f in kitchen_name_only]
+living = [os.path.join(living_path, f) for f in living_name_only]
+bedroom = [os.path.join(bedroom_path, f) for f in bedroom_name_only]
+bathroom = [os.path.join(bathroom_path, f) for f in bathroom_name_only]
+#
+imgs = kitchen + living + bedroom + bathroom
+depths = [f.replace("/images/", "/depths/").replace('.png', '.p') for f in imgs]
+depths = ['/'.join(d.split('/')[:-1]) + '/depth' + d.split('/')[-1] for d in depths]
+
+
+hors = [f.replace("/images/", "/hors/").replace(".png", ".p") for f in imgs]
+hors = ['/'.join(d.split('/')[:-1]) + '/horizon' + d.split('/')[-1] for d in hors]
+
+
+
+
+identifiers_broken_img =[]
+identifiers_broken_mask =[]
+identifiers_broken_meta =[]
+
+paths_broken_img =[]
+paths_broken_mask =[]
+paths_broken_meta =[]
+
+counter = 0
+
+for img_path in imgs:
+	counter +=1
+	if counter %1000 == 0:
+		print("counter is ", counter)
+	identifier = img_path[0].split('/')[-1][:-4]
+	#Try opening each of them 
+	try:
+		img = Image.open(img_path).convert("RGB")
+	except:
+		paths_broken_img.append(img_path)
+
+	
+
+pickle.dump(paths_broken_img, open(os.path.join(args.data_path, "paths_broken_img.p") , "wb"))
+
+
+#Get identifiers
+
+dict_identifier = {'kitchen':[], 'living':[], 'bedroom':[], 'bathroom':[]}
+
+for i, p in enumerate(paths_broken_img):
+	room_type = p.split('/')[-3]
+	identifier = p.split('/')[-1].split('.')[0]
+	dict_identifier[room_type].append(identifier)
+
+
+for room_type in dict_identifier:
+	dict_identifier[room_type]= list(set(dict_identifier[room_type]))
+
+
+#Remove all the broken ones
+#get paths first
+delete_images = []; delete_masks=[]; delete_metas=[]
+for r, identifier_list in dict_identifier.items():
+	for identifier in identifier_list:
+		#Remove image, mask, meta
+		folder_path = os.path.join(root, r, 'images')
+		file_path = identifier + '.png'
+		delete_images.append(os.path.join(folder_path, file_path))
+		#
+		folder_path = os.path.join(root, r, 'masks')
+		file_path = identifier + '.png'
+		delete_masks.append(os.path.join(folder_path, file_path))
+		#
+		folder_path = os.path.join(root, r, 'meta')
+		file_path = identifier + '.json'
+		delete_metas.append(os.path.join(folder_path, file_path))
+
+for p1, p2, p3 in zip(delete_images,delete_masks, delete_metas):
+	assert os.path.exists(p1); assert os.path.exists(p2); assert os.path.exists(p3)
+	os.remove(p1); os.remove(p2); os.remove(p3)
+
