f73cb8e228e6a130e33fec556d8b8b28cf554b4f
So Yeon Min <soyeonmin@Soui-MacBookPro.local>
2022-04-24 23:29:27 -0400

    m

--- scripts/train_mrcnn.py
+++ scripts/train_mrcnn.py
@@ -178,10 +178,11 @@ class AlfredDataset(object):
 
         if not(train_dataset):
             #just keep 1000
-            ka = int(len(kitchen) / 1000)
-            la = int(len(living) / 1000)
-            bea = int(len(bedroom) / 1000)
-            baa = int(len(bathroom) / 1000)
+            print("Total is ", len(kitchen+living + bedroom + bathroom))
+            ka = int(len(kitchen) / 10000)
+            la = int(len(living) / 2000)
+            bea = int(len(bedroom) / 2000)
+            baa = int(len(bathroom) / 2000)
             kitchen = [k for i,k in enumerate(kitchen) if i % ka == 0]
             living = [k for i,k in enumerate(living) if i % la == 0]
             bedroom = [k for i,k in enumerate(bedroom)]# if i % bea == 0]
