37f9281f0f756649a2f6b495528f2ee282320456
So Yeon Min <soyeonmin@Sos-MacBook-Pro.local>
2024-02-09 00:50:23 -0500

    m

--- scripts/train_mrcnn.py
+++ scripts/train_mrcnn.py
@@ -70,11 +70,17 @@ class AlfredDataset(object):
         return [f for f in file_list if f[-4:] == '.png']
 
     def get_data_files(self, root, balance_scenes=False, train_dataset=False):
-        #rgb_dirs = glob(root + "/*/rgb_*/*")
-        #mask_dirs = [rgb_path.replace('/rgb_', '/masks_').replace('.png', '.p') for rgb_path in rgb_dirs]
-        self.imgs = glob(root + "/*/rgb_*/*")
-        self.masks = [rgb_path.replace('/rgb_', '/masks_').replace('.png', '.p') for rgb_path in self.imgs]
+        rgb_dirs = glob(root + "/*/rgb_*/*")
+        mask_dirs = [rgb_path.replace('/rgb_', '/masks_').replace('.png', '.p') for rgb_path in rgb_dirs]
+        #self.imgs = glob(root + "/*/rgb_*/*")
+        #self.masks = [rgb_path.replace('/rgb_', '/masks_').replace('.png', '.p') for rgb_path in self.imgs]
 
+        self.imgs = []
+        self.masks = []
+        for rgb, mask in zip(rgb_dirs, mask_dirs):
+            if os.path.exists(rgb) and os.path.exists(mask):
+                self.imgs.append(rgb)
+                self.masks.append(mask) 
 
          
 
